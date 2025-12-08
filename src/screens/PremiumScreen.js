404eea774c9bcd96106e21e52e095d2dfb930dfe
nathandavo <nathandavo@icloud.com>
2025-12-08 20:17:08 +0000

    Update PremiumScreen.js

--- src/screens/PremiumScreen.js
+++ src/screens/PremiumScreen.js
@@ -1,4 +1,3 @@
-console.log("🔍 FRONTEND API URL:", API_URL);
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet, Linking } from "react-native";
 import { API_URL } from "../../App"; // Make sure this points to your backend
